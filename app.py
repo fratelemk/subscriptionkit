876d8926a1a5bedb38a10cfa9fd87025de555137
Mihai Florescu <fratelemk@outlook.com>
2025-04-06 14:22:25 +0300

    added icloud service

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
+import os
+import sys
 import re
 import emoji
 import pandas as pd
 import streamlit as st
 from currency_converter import CurrencyConverter
+from pyicloud import PyiCloudService
+
+api = PyiCloudService(os.environ["APPLE_ID"], os.environ["PASSWORD"])
+if api.requires_2fa:
+    print("Two-factor authentication required.")
+    code = input("Enter the code you received of one of your approved devices: ")
+    result = api.validate_2fa_code(code)
+    print("Code validation result: %s" % result)
+
+    if not result:
+        print("Failed to verify security code")
+        sys.exit(1)
+
+    if not api.is_trusted_session:
+        print("Session is not trusted. Requesting trust...")
+        result = api.trust_session()
+        print("Session trust result %s" % result)
+
+        if not result:
+            print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
+
+file = api.drive["Subscriptions.csv"]
 
 currency_converter = CurrencyConverter(verbose=False)
 
@@ -14,33 +38,18 @@ def apply_conversion(row: pd.Series, new_currency):
 
     return f"{converted_rate:.2f} {new_currency}"
 
-conn = st.connection('testing', type="sql")
 
+if file:
+    with file.open(stream=True) as response:
+        df = pd.read_csv(filepath_or_buffer=response.raw)
+        currency_options = ("🇺🇸USD", "🇬🇧GBP", "🇪🇺EUR", "🇷🇴RON")
+        currency = st.selectbox('Currency', currency_options)
+        currency_value = re.sub(r'\s+', '', emoji.replace_emoji(currency, replace=''))
 
-# from queries import SELECT, VIEW, CREATE
-# from datetime import date
-# from sqlalchemy import text
+        df['Amount'] = df.apply(lambda row: apply_conversion(row, currency_value), axis=1)
+        df.drop("Currency", axis=1, inplace=True)
 
-# with conn.session as s:
-#     s.execute(text(CREATE))
-#     s.execute(text(VIEW))
-#
-#     s.commit()
-#
-#     subscriptions: pd.DataFrame = conn.query(SELECT)
-#     subscriptions['Active'] = subscriptions['Active'].astype(bool)
-#     subscriptions['Renewal'] = subscriptions['Renewal'].apply(lambda x: date.fromtimestamp(x).strftime('%d %B'))
-#
-#     st.write(subscriptions)
-
-f = st.file_uploader("Upload")
-if f:
-    df = pd.read_csv(filepath_or_buffer=f)
-    currency_options = ("🇺🇸USD", "🇬🇧GBP", "🇪🇺EUR", "🇷🇴RON")
-    currency = st.selectbox('Currency', currency_options)
-    currency_value = re.sub(r'\s+', '', emoji.replace_emoji(currency, replace=''))
-
-    df['Amount'] = df.apply(lambda row: apply_conversion(row, currency_value), axis=1)
-    df.drop("Currency", axis=1, inplace=True)
+        total = df['Amount'].apply(lambda x: float(x.removesuffix(currency_value).strip())).sum()
 
     st.dataframe(df)
+    st.metric(label="Total", value=f"{total:.2f} {currency_value}")
